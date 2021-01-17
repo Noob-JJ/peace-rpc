921c62b3cecd99dd05b0cf65e98d6970f188dee3
jiangjie <2775904687@qq.com>
2021-01-17 15:56:51 +0800

    start moudle

---
+++ rpc-net/src/main/java/config/ConfigTem.java
@@ -0,0 +1,60 @@
+package config;
+
+/**
+ * Created by JackJ on 2021/1/17.
+ */
+public final class ConfigTem {
+
+    private final String registry;
+
+    private final String registryHost;
+
+    private final String providerIp;
+
+    private final String providerPort;
+
+    private final String providerClass;
+
+    private final String subcribeClass;
+
+    private final String name;
+
+
+    public ConfigTem(String registry, String registryHost, String providerIp, String providerPort, String providerClass, String subcribeClass, String name) {
+        this.name = name == null ? "defaultName" : name;
+        this.registry = registry == null ? "zookeeper" : registry;
+        this.registryHost = registryHost == null ? "" : registryHost;
+        this.providerIp = providerIp == null ? "" : providerIp;
+        this.providerPort = providerPort == null ? "" : "2181";
+        this.providerClass = providerClass == null ? "" : providerClass;
+        this.subcribeClass = subcribeClass == null ? "" : subcribeClass;
+    }
+
+    public String getRegistry() {
+        return registry;
+    }
+
+    public String getRegistryHost() {
+        return registryHost;
+    }
+
+    public String getProviderPort() {
+        return providerPort;
+    }
+
+    public String getProviderClass() {
+        return providerClass;
+    }
+
+    public String getSubcribeClass() {
+        return subcribeClass;
+    }
+
+    public String getProviderIp() {
+        return providerIp;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- rpc-net/src/main/java/start/Rpc.java
+++ rpc-net/src/main/java/start/Rpc.java
@@ -1,9 +1,82 @@
 package start;
 
+import config.Config;
+import config.ConfigTem;
+import config.SimpleConfig;
+import regsitry.Registry;
+import regsitry.RegistryFactory;
+import util.FileUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.Properties;
+
 /**
  * Created by JackJ on 2021/1/16.
  */
 public class Rpc {
-    // TODO: 2021/1/16 建造者模式进行一些参数构造
 
+    public static ConfigTem config;
+
+    private static Rpc instace = new Rpc();
+
+    private Rpc(){
+
+    }
+
+    private void init() throws Exception {
+        URL url = FileUtils.loadFile("META_INF/rpc.properties");
+        Properties properties = new Properties();
+        properties.load(url.openStream());
+        SimpleConfig.INSTANCE.init(properties);
+        changeConfig();
+        registry();
+        sub();
+    }
+
+    private void sub() throws Exception {
+        Registry registry = RegistryFactory.getRegistry();
+        String[] classNames = config.getSubcribeClass().split(",");
+        registry.subscribe(classNames);
+    }
+
+    private void registry() throws Exception {
+        Registry registry = RegistryFactory.getRegistry();
+        String[] classNames = config.getProviderClass().split(",");
+        String name = config.getName();
+        String port = config.getProviderPort();
+        String ip = config.getProviderIp();
+        for (String className : classNames) {
+            registry.registry(name, className, ip, port);
+        }
+    }
+
+    private void changeConfig() throws UnknownHostException {
+        Config simpleConfig = SimpleConfig.INSTANCE;
+        InetAddress addr = InetAddress.getLocalHost();
+        String registry = simpleConfig.get("rpc.registry");
+        String registryHost = simpleConfig.get("rpc.registry.host");
+        String providerIp = addr.getHostAddress();
+        String providerPort = simpleConfig.get("rpc.provider.port");
+        String providerClass = simpleConfig.get("rpc.provider.class");
+        String subcribeClass = simpleConfig.get("rpc.subcribe.class");
+        String name = simpleConfig.get("rpc.name");
+        config = new ConfigTem(registry, registryHost, providerIp, providerPort, providerClass, subcribeClass, name);
+    }
+
+    public static Rpc getInstace() throws Exception {
+        if (instace == null) {
+            // TODO: 2021/1/17 这里为什么会报错 单例模式不就是这么实现的吗
+            synchronized ("lock") {
+                if (instace == null) {
+                    instace = new Rpc();
+                    instace.init();
+                }
+            }
+        }
+        return instace;
+    }
 }
