06e7c296861ad9ba0a8df9e2f3d28dcc84f3d0f1
jiangjie <2775904687@qq.com>
2021-03-06 22:14:48 +0800

    error handle.

--- consumer/src/main/java/Main.java
+++ consumer/src/main/java/Main.java
@@ -4,9 +4,7 @@ import start.Rpc;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Rpc rpc = Rpc.getInstance();
 
-        Test t = rpc.getBean(Test.class);
 
     }
 
--- provider/src/main/java/Main.java
+++ provider/src/main/java/Main.java
@@ -2,6 +2,5 @@ import start.Rpc;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Rpc rpc = Rpc.getInstance();
     }
 }
